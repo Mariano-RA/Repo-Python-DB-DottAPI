b017403b92ac01dfbb10e73350c0e0f7b6f76585
Mariano RODRIGUEZ ARCE <mrodriguezarce@justiciacordoba.gob.ar>
2023-08-23 17:03:20 -0300

    - a

--- Dott-Python-App.py
+++ Dott-Python-App.py
@@ -3,7 +3,7 @@ from openpyxl import load_workbook
 from openpyxl.formula.translate import Translator
 from openpyxl.utils import get_column_letter
 from openpyxl.utils import FORMULAE
-
+import xlwings as xw
 import pandas as pd
 import csv
 import json
@@ -12,12 +12,12 @@ import os
 app = Flask(__name__)
 
 # Direccion archivos
-listadosTemporales = 'Repo-Python-DB-DottAPI\\archivosPorCargar\\temporales\\'
-listadoCsv = 'Repo-Python-DB-DottAPI\\archivosPorCargar\\csv\\'
-listadoJson = 'Repo-Python-DB-DottAPI\\archivosPorCargar\\archivosJson\\'
-diccionarios = 'Repo-Python-DB-DottAPI\\nuevosScripts\\diccionarios\\diccionarios.json'
+listadosTemporales = 'archivosPorCargar\\temporales\\'
+listadoCsv = 'archivosPorCargar\\csv'
+listadoJson = 'archivosPorCargar\\archivosJson\\'
+diccionarios = 'nuevosScripts\diccionarios\diccionarios.json'
 
-# Función para encontrar un valor en el dicc¶ionario
+# Función para encontrar un valor en el diccionario
 
 
 def encontrar_valor(diccionario, clave):
@@ -48,30 +48,27 @@ def procesar_archivo_air():
     archivo.save(listadosTemporales+"air.csv")
 
     # Función para crear el archivo JSON
-    def crearArchivoJson():
+
+    with open(listadosTemporales+"air.csv", 'r') as file:
+        csv_reader = csv.reader(file, delimiter=',')
+        next(csv_reader)  # Ignora la primera fila de encabezados
         data = []
-        with open(listadosTemporales+"air.csv", 'r') as file:
-            csv_reader = csv.reader(file, delimiter=',')
-            next(csv_reader)  # Ignora la primera fila de encabezados
-            for row in csv_reader:
-                if row[2] != "A":
-                    descripcion = row[1]
-                    rubro = row[10]
-                    iva = row[4]
-                    precio = row[2]
-                    registro = {
-                        'proveedor': 'air',
-                        'producto': descripcion,
-                        'categoria': encontrar_valor(obtenerDiccionario('air'), rubro),
-                        'precio': round((float(precio) * (1 + (float(iva) / 100)) * 1.1))
-                    }
-                    data.append(registro)
+        for row in csv_reader:
+            if row[2] != "A":
+                descripcion = row[1]
+                rubro = row[10]
+                iva = row[4]
+                precio = row[2]
+                registro = {
+                    'proveedor': 'air',
+                    'producto': descripcion,
+                    'categoria': encontrar_valor(obtenerDiccionario('air'), rubro),
+                    'precio': round((float(precio) * (1 + (float(iva) / 100)) * 1.1))
+                }
+                data.append(registro)
 
-        with open(listadoJson+"air.json", 'w') as jf:
-            json.dump(data, jf, ensure_ascii=False, indent=2)
-
-    # Ejecutar el proceso de creación del JSON
-    crearArchivoJson()
+    with open(listadoJson+"air.json", 'w') as jf:
+        json.dump(data, jf, ensure_ascii=False, indent=2)
 
     return jsonify({'message': 'Archivo CSV procesado y JSON creado correctamente'})
 
@@ -84,14 +81,14 @@ def procesar_archivo_eikon():
     if archivo.filename == '':
         return jsonify({'error': 'No se ha seleccionado un archivo'}), 400
 
-    # Guardar el archivo temporalmente
     archivo.save(listadosTemporales+'temp_eikon.xlsx')
 
-    # Resto del proceso para procesar archivos Eikon
     df = pd.read_excel(listadosTemporales+'temp_eikon.xlsx')
-    # Se utilizan los índices 0, 1 y 2 para las primeras tres filas
+
+    # Se borran las primeras filas
     df = df.drop([0, 1, 2])
     df.reset_index(drop=True, inplace=True)
+
     # Guardar como CSV
     df.to_csv(listadoCsv+'listadoEik.csv', index=False)
 
@@ -120,9 +117,6 @@ def procesar_archivo_eikon():
             # Agrega el diccionario a la lista de datos
             data.append(registro)
 
-    # Convierte la lista de datos a formato JSON
-    # json_data = json.dumps(data, indent=4)
-
     with open(listadoJson+'listadoEik.json', 'w') as jf:
         json.dump(data, jf, ensure_ascii=False, indent=2)
 
@@ -137,12 +131,8 @@ def procesar_archivo_elit():
     if archivo.filename == '':
         return jsonify({'error': 'No se ha seleccionado un archivo'}), 400
 
-    # Guardar el archivo temporalmente
     archivo.save(listadosTemporales+'temp_elit.xlsx')
-
-    # Resto del proceso para procesar archivos Eikon
     df = pd.read_excel(listadosTemporales+'temp_elit.xlsx')
-
     df.to_csv(listadoCsv+'listadoElit.csv', index=False)
 
     # Abre el archivo CSV en modo lectura con la codificación adecuada
@@ -152,8 +142,6 @@ def procesar_archivo_elit():
 
         # Crea una lista para almacenar los datos
         data = []
-
-        # Lee cada fila del archivo CSV (ignorando la primera fila de encabezados)
 
         next(csv_reader)  # Ignora la primera fila de encabezados
         for row in csv_reader:
@@ -180,30 +168,27 @@ def procesar_archivo_elit():
     return jsonify({'message': 'Archivo Elit procesado correctamente'})
 
 
+def obtenerTipoIva(clave):
+    # Diccionario con tipo de IVA
+    tipoIva = {
+        "002-I.V.A. 10.5 %": 10.5,
+        "001-I.V.A. 21 %": 21,
+        "005-Impuestos Internos": 21
+    }
+    if clave in tipoIva:
+        return tipoIva[clave]
+
+
 @app.route('/procesar_archivo_hdc', methods=['POST'])
 def procesar_archivo_hdc():
-
-    def obtenerTipoIva(clave):
-        # Diccionario con tipo de IVA
-        tipoIva = {
-            "002-I.V.A. 10.5 %": 10.5,
-            "001-I.V.A. 21 %": 21,
-            "005-Impuestos Internos": 21
-        }
-        if clave in tipoIva:
-            return tipoIva[clave]
 
     archivo = request.files['file']
 
     if archivo.filename == '':
         return jsonify({'error': 'No se ha seleccionado un archivo'}), 400
 
-    # Guardar el archivo temporalmente
     archivo.save(listadosTemporales+'temp_hdc.xlsx')
-
-    # Resto del proceso para procesar archivos Eikon
     df = pd.read_excel(listadosTemporales+'temp_hdc.xlsx')
-
     df.to_csv(listadoCsv+'listadoHdc.csv', index=False)
 
     # Abre el archivo CSV en modo lectura con la codificación adecuada
@@ -213,8 +198,6 @@ def procesar_archivo_hdc():
 
         # Crea una lista para almacenar los datos
         data = []
-
-        # Lee cada fila del archivo CSV (ignorando la primera fila de encabezados)
 
         next(csv_reader)  # Ignora la primera fila de encabezados
 
@@ -255,7 +238,6 @@ def procesar_archivo_invid():
 
     # Guardar el archivo .xls en el directorio
     archivo.save(listadosTemporales + 'temp_invid.xlsx')
-
     df = pd.read_excel(listadosTemporales+'temp_invid.xlsx')
 
     # Se utilizan los índices 0, 1 y 2 para las primeras tres filas
@@ -265,51 +247,41 @@ def procesar_archivo_invid():
 
     # Cargar el libro de trabajo
     book = load_workbook(listadosTemporales + 'temp_invid.xlsx')
-    sheet = book.active
-    # Cambia esta fórmula según tus necesidades
+    sheet = book['Sheet1']
 
     sheet['I3'] = "categoria"
-    # # Define la fórmula
-    # formula = '=IF(LEN(H4)<2,"",IF(AND(LEN(A2)<2,LEN(C2)<2),B2,I3))'
-    # # Obtén la letra de la columna 'I'
-    # column_letter = get_column_letter(9)  # 9 es el número de la columna 'I'
+    # Obtén la letra de la columna 'I'
+    column_letter = get_column_letter(9)
 
-    # # Aplica la fórmula en las celdas de la columna 'I' (desde la fila 5 en adelante)
-    # for row_num in range(4, sheet.max_row + 1):
-    #     formula = f'=IF(LEN(H{row_num})<2,"",IF(AND(LEN(A{row_num-2})<2,LEN(C{row_num-2})<2),B{row_num-2},{column_letter}{row_num-1}))'
-    #     cell_reference = f'{column_letter}{row_num}'
-    #     sheet[cell_reference] = formula
+    # Aplica la fórmula en las celdas de la columna 'I' (desde la fila 5 en adelante)
+    for row_num in range(4, sheet.max_row + 1):
+        formula_row = f'=IF(LEN(H{row_num})<2,"",IF(AND(LEN(A{row_num-2})<2,LEN(C{row_num-2})<2),B{row_num-2},{column_letter}{row_num-1}))'
+        cell_reference = f'{column_letter}{row_num}'
+        sheet[cell_reference] = formula_row
 
-    def transform_formula(h_value, a_value, c_value, b_value, i_value):
-        if h_value is None:
-            return ""
-        if len(h_value) < 2:
-            return ""
-        if a_value is None or c_value is None:
-            return ""
-        if len(a_value) < 2 and len(c_value) < 2:
-            return b_value
-        return i_value
+    book.save(listadosTemporales + 'temp_invid.xlsx')
+    book.close()
 
-    # Iterar a través de las filas de la columna "I"
-    # Suponiendo que tus datos comienzan desde la fila 2 y hay 9 columnas
-    for fila in sheet.iter_rows(min_row=2, max_col=9, max_row=sheet.max_row):
-        h3 = fila[7].value  # Columna H
-        a1 = fila[0].value  # Columna A
-        c1 = fila[2].value  # Columna C
-        b1 = fila[1].value  # Columna B
-        i1 = fila[8].value  # Columna I
+    app = xw.App(visible=False)  # Abre Excel en segundo plano
+    libro = app.books.open(listadosTemporales + 'temp_invid.xlsx')
+    hoja = libro.sheets["Sheet1"]
 
-        resultado = transform_formula(h3, a1, c1, b1, i1)
+    # Encuentra el último número de fila en la columna 'I'
+    ultima_fila = hoja.range("I" + str(hoja.cells.last_cell.row)).end("up").row
 
-        # Asignar el resultado a la columna I en la misma fila
-        # Si el resultado es una cadena vacía, asignar None
-        fila[8].value = resultado if resultado != "" else None
+    # Empieza en la fila 2 para omitir encabezados
+    for fila_numero in range(2, ultima_fila + 1):
+        celda_formula = hoja.range("I" + str(fila_numero))
+        resultado = celda_formula.value
+        celda_formula.value = resultado
 
-    # Crea una lista para almacenar los datos de las celdas
-    book.save(listadosTemporales + 'temp_invid_test.xlsx')
+    # Cierra el archivo Excel
+    libro.save()
+    libro.close()
+    app.quit()
 
     # Guardar como CSV
+    df = pd.read_excel(listadosTemporales+'temp_invid.xlsx')
     df.to_csv(listadoCsv+'listadoInvid.csv', index=False)
 
     with open(listadoCsv+'listadoInvid.csv', 'r') as file:
@@ -345,6 +317,142 @@ def procesar_archivo_invid():
         json.dump(data, jf, ensure_ascii=False, indent=2)
 
     return jsonify({'message': 'Archivo invid procesado correctamente'})
+
+
+@app.route('/procesar_archivo_nb', methods=['POST'])
+def procesar_archivo_nb():
+
+    archivo = request.files['file']
+
+    if archivo.filename == '':
+        return jsonify({'error': 'No se ha seleccionado un archivo'}), 400
+
+    # Guardar el archivo temporalmente
+    archivo.save(listadoCsv+"listadoNb.csv")
+
+    # Abre el archivo CSV en modo lectura con la codificación adecuada
+    with open(listadoCsv+"listadoNb.csv", 'r', encoding="utf8") as file:
+        # Crea un objeto lector CSV
+        csv_reader = csv.reader(file, delimiter=';')
+
+        # Crea una lista para almacenar los datos
+        data = []
+
+        # Lee cada fila del archivo CSV (ignorando la primera fila de encabezados)
+
+        next(csv_reader)  # Ignora la primera fila de encabezados
+        for row in csv_reader:
+            descripcion = row[3]
+            categoria = row[2]
+            precio = row[10]
+
+            # Crea un diccionario con los datos de cada registro
+            registro = {
+                'proveedor': 'nb',
+                'producto': descripcion,
+                'categoria': encontrar_valor(obtenerDiccionario('nb'), categoria),
+                'precio': round((float(precio) * 1.1))
+            }
+
+            # Agrega el diccionario a la lista de datos
+            data.append(registro)
+
+    with open(listadoJson+"listadoNb.json", 'w') as jf:
+        json.dump(data, jf, ensure_ascii=False, indent=2)
+
+    return jsonify({'message': 'Archivo nb procesado correctamente'})
+
+
+@app.route('/procesar_archivo_mega', methods=['POST'])
+def procesar_archivo_mega():
+
+    archivo = request.files['file']
+
+    if archivo.filename == '':
+        return jsonify({'error': 'No se ha seleccionado un archivo'}), 400
+
+    # Guardar el archivo .xls en el directorio
+    archivo.save(listadosTemporales + 'temp_mega_test.xlsx')
+    df = pd.read_excel(listadosTemporales+'temp_mega_test.xlsx')
+
+    # Se utilizan los índices 0, 1 y 2 para las primeras tres filas
+    df = df.drop([0, 1])
+    df.reset_index(drop=True, inplace=True)
+    df.to_excel(listadosTemporales + 'temp_mega_test.xlsx', index=False)
+
+    # Cargar el libro de trabajo
+    book = load_workbook(listadosTemporales + 'temp_mega_test.xlsx')
+    sheet = book['Sheet1']
+
+    sheet['G2'] = "categoria"
+    column_letter = get_column_letter(7)
+
+    # Aplica la fórmula en las celdas de la columna 'G' (desde la fila 5 en adelante)
+    for row_num in range(3, sheet.max_row + 1):
+        formula = f'=IF(AND(ISBLANK(B{row_num-2}), ISBLANK(B{row_num - 1})), CONCAT(A{row_num-2}," - ",A{row_num - 1}), IF(ISBLANK(E{row_num}), "", IF(LEN(B{row_num - 1}) < 2, CONCAT(LEFT({column_letter}{row_num - 2}, FIND(" - ", {column_letter}{row_num - 2}) - 1), " - ", A{row_num - 1}), {column_letter}{row_num-1})))'
+        cell_reference = f'{column_letter}{row_num}'
+        sheet[cell_reference] = formula
+
+    book.save(listadosTemporales + 'temp_mega_test.xlsx')
+    book.close()
+
+    app = xw.App(visible=False)  # Abre Excel en segundo plano
+    libro = app.books.open(listadosTemporales + 'temp_mega_test.xlsx')
+    hoja = libro.sheets["Sheet1"]
+
+    # Encuentra el último número de fila en la columna 'I'
+    ultima_fila = hoja.range("G" + str(hoja.cells.last_cell.row)).end("up").row
+
+    # Empieza en la fila 2 para omitir encabezados
+    for fila_numero in range(3, ultima_fila + 1):
+        celda_formula = hoja.range("G" + str(fila_numero))
+        print(celda_formula.value)
+        resultado = celda_formula.value
+        celda_formula.value = resultado
+
+    # # Cierra el archivo Excel
+    libro.save()
+    libro.close()
+    app.quit()
+
+    # Guardar como CSV
+    # df = pd.read_excel(listadosTemporales+'temp_mega.xlsx')
+    df.to_csv(listadoCsv+'listadoMega.csv', index=False)
+
+    # Abre el archivo CSV en modo lectura con la codificación adecuada
+    with open(listadoCsv+"listadoMega.csv", 'r', encoding="utf-8") as file:
+        # Crea un objeto lector CSV
+        csv_reader = csv.reader(file, delimiter=',')
+
+        # Crea una lista para almacenar los datos
+        data = []
+
+        # Lee cada fila del archivo CSV (ignorando la primera fila de encabezados)
+
+        next(csv_reader)  # Ignora la primera fila de encabezados
+        for row in csv_reader:
+            if (row[3] != ""):
+                descripcion = row[1]
+                categoria = row[6]
+                iva = row[4].replace("+", "").replace("%", "")
+                precio = row[2].replace("U$s ", "")
+
+                # Crea un diccionario con los datos de cada registro
+                registro = {
+                    "proveedor": "mega",
+                    'producto': descripcion,
+                    'categoria': encontrar_valor(obtenerDiccionario('mega'), categoria),
+                    'precio': round((float(precio) * (1 + (float(iva)/100)) * 1.1))
+                }
+
+                # Agrega el diccionario a la lista de datos
+
+                data.append(registro)
+
+    with open(listadoJson+"listadoMega.json", 'w') as jf:
+        json.dump(data, jf, ensure_ascii=False, indent=2)
+
+    return jsonify({'message': 'Archivo nb procesado correctamente'})
 
 
 if __name__ == '__main__':
